0d8ec15996cf70b4bcdc84872f31dc67cfb9f356
Jannik Prinz <jannik.prinz@gmail.com>
2019-09-08 12:18:29 +0200

    Add interactive testing of trained function. Improved layout of neural network to approximate the function.

--- source/torchtest/neuralnetwork.cpp
+++ source/torchtest/neuralnetwork.cpp
@@ -4,15 +4,15 @@ namespace TorchTest {
 
 Network::Network()
 {
-  fc1 = register_module("fc1", torch::nn::Linear(1, 10));
-  fc2 = register_module("fc2", torch::nn::Linear(10, 10));
+  fc1 = register_module("fc1", torch::nn::Linear(1, 20));
+  fc2 = register_module("fc2", torch::nn::Linear(20, 10));
   fc3 = register_module("fc3", torch::nn::Linear(10, 1));
 }
 
 torch::Tensor Network::forward(torch::Tensor x)
 {
-  x = torch::relu(fc1->forward(x));
-  x = torch::relu(fc2->forward(x));
+  x = torch::sigmoid(fc1->forward(x));
+  x = torch::sigmoid(fc2->forward(x));
   x = torch::relu(fc3->forward(x));
   return x;
 }
--- source/torchtest/torchtest.cpp
+++ source/torchtest/torchtest.cpp
@@ -4,11 +4,11 @@ namespace TorchTest {
 
 namespace {
 
-const size_t NUMBER_OF_EPOCHS{10000};
-const size_t SIZE_OF_TEST_DATA{2};
-const double LOWER_BOUND_OF_TEST_RANGE{0};
-const double UPPER_BOUND_OF_TEST_RANGE{4};
-const double LEARNING_RATE{0.001};
+const size_t NUMBER_OF_EPOCHS{1000};
+const size_t SIZE_OF_TEST_DATA{20};
+const double LOWER_BOUND_OF_TEST_RANGE{1};
+const double UPPER_BOUND_OF_TEST_RANGE{5};
+const double LEARNING_RATE{0.0001};
 
 }
 
@@ -32,8 +32,6 @@ void TorchTest::run()
 
       auto prediction = network.forward(x);
 
-
-
 //      prediction = prediction.toType(torch::ScalarType::Long);
 //      auto target = y.toType(torch::ScalarType::Long);
       auto loss = torch::mse_loss(prediction, y);
@@ -42,17 +40,31 @@ void TorchTest::run()
       loss.backward();
       optimizer.step();
 
-      if (epoch == 1 || epoch == NUMBER_OF_EPOCHS) {
+      if (x.item<TensorDataType>() == LOWER_BOUND_OF_TEST_RANGE && (epoch == 1 || epoch == NUMBER_OF_EPOCHS)) {
         std::cout << "Epoch: " << epoch << std::endl;
         std::cout << "x: " << x.item<TensorDataType>() << std::endl;
         std::cout << "y: " << y.item<TensorDataType>() << std::endl;
         std::cout << "prediction: " << prediction.item<TensorDataType>() << std::endl;
-        std::cout << "loss: " << loss.item<double>() << std::endl;
+        std::cout << "loss: " << loss.item<double>() << std::endl << std::endl;
       }
     }
   }
 
-  // TODO interactive testing of the network
+  bool stopTest = false;
+  while (!stopTest) {
+    std::cout << "Input x: ";
+
+    std::string input;
+    std::cin >> input;
+
+    try {
+      auto in = static_cast<TensorDataType>(std::stod(input));
+      auto out = network.forward(torch::full(1, in));
+      std::cout << "y: " << TargetFunction(in) << " prediction: " << out.item<TensorDataType>() << std::endl;
+    } catch (std::exception&) {
+      stopTest = true;
+    }
+  }
 }
 
 void TorchTest::initialize()
@@ -60,13 +72,16 @@ void TorchTest::initialize()
   TensorDataType stepSize = (UPPER_BOUND_OF_TEST_RANGE - LOWER_BOUND_OF_TEST_RANGE) / (SIZE_OF_TEST_DATA - 1);
   TensorDataType x = LOWER_BOUND_OF_TEST_RANGE;
 
+  std::cout << "x values of the testdata: ";
   for (size_t i = 0; i < SIZE_OF_TEST_DATA; ++i) {
-    auto xt = torch::full((1), x, torch::ScalarType::Float);
-    auto yt = torch::full((1), TargetFunction(x), torch::ScalarType::Float);
+    std::cout << x << " ";
+    auto xt = torch::full(1, x);
+    auto yt = torch::full(1, TargetFunction(x));
 
     testData.emplace_back(xt, yt);
     x += stepSize;
   }
+  std::cout << std::endl;
 
   std::cout << "Initialization finished!" << std::endl;
 }
